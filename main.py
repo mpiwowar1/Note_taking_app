0e653692368c59a2a5789c8a4ee1dfab8398c6b5
mpiwowar <maksymilian.piwowar6@gmail.com>
2025-10-14 10:59:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import customtkinter as ct
 
-def ButtonCallBack() :
-    print("button pressed")
+class App(ct.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("NoteApp")
+        self.geometry("400x180")
+        self.grid_columnconfigure((0,1),weight=1)
+        self.grid_rowconfigure((0, 1), weight=1)
 
-app=ct.CTk()
+        self.button = ct.CTkButton(self,text="my button",command=self.ButtonCallBack)
+        self.button.grid(row=2, column=0, padx=10,pady=10,sticky="ew",columnspan=2)
 
-app.title("NoteApp")
-app.geometry("1000x600")
+        self.checkbox_1 = ct.CTkCheckBox(self,text="checkbox1")
+        self.checkbox_1.grid(row=0,column=0,padx=10,pady=(10,0),sticky="w")
 
-button = ct.CTkButton(app,text="my button",command=ButtonCallBack)
-button.grid(row=0, column=0, padx=20,pady=20,sticky="ew")
-app.grid_columnconfigure(0,weight=1)
+        self.checkbox_2 = ct.CTkCheckBox(self,text="checkbox2")
+        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10,0), sticky="w")
+        self.grid_columnconfigure(0,weight=1)
+
+    def ButtonCallBack(self) :
+        print("button pressed")
 
 
+app=App()
 app.mainloop()
+
+
+
